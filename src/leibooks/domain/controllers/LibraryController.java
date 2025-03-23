da08498b62e51b96108beee0480f482a5df776eb
“EricksonDagama” <eriksongamamorais@hotmail.com>
2025-03-23 19:14:41 +0000

    1. Implementação de LibraryController

--- src/leibooks/domain/controllers/LibraryController.java
+++ src/leibooks/domain/controllers/LibraryController.java
@@ -1,85 +1,72 @@
 package leibooks.domain.controllers;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 import leibooks.domain.core.ILibrary;
+import leibooks.domain.core.DocumentFactory;
 import leibooks.domain.facade.DocumentProperties;
 import leibooks.domain.facade.IDocument;
 import leibooks.domain.facade.ILibraryController;
 import leibooks.domain.facade.events.DocumentEvent;
 import leibooks.domain.facade.events.LBEvent;
+import leibooks.utils.AbsSubject;
 import leibooks.utils.Listener;
 
-public class LibraryController implements ILibraryController{
+public class LibraryController extends AbsSubject<LBEvent> implements ILibraryController {
 
-	public LibraryController(ILibrary library) {
-		// TODO Auto-generated constructor stub
-	}
+    private final ILibrary library;
 
+    public LibraryController(ILibrary library) {
+        this.library = library;
+        this.library.registerListener(this); // Para receber eventos dos documentos
+    }
 
-	@Override
-	public void emitEvent(LBEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Iterable<IDocument> getDocuments() {
+        return library;
+    }
 
-	@Override
-	public void registerListener(Listener<LBEvent> obs) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Optional<IDocument> importDocument(String title, String pathTofile) {
+        try {
+            IDocument doc = DocumentFactory.INSTANCE.createDocument(title, pathTofile);
+            if (library.addDocument(doc)) {
+                return Optional.of(doc);
+            }
+        } catch (FileNotFoundException e) {
+            // erro ao importar documento
+        }
+        return Optional.empty();
+    }
 
-	@Override
-	public void unregisterListener(Listener<LBEvent> obs) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void removeDocument(IDocument document) {
+        library.removeDocument(document);
+    }
 
-	@Override
-	public void processEvent(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void updateDocument(IDocument document, DocumentProperties documentProperties) {
+        library.updateDocument(document, documentProperties);
+    }
 
-	@Override
-	public Iterable<IDocument> getDocuments() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<IDocument> getMatches(String regex) {
+        return library.getMatches(regex);
+    }
 
-	@Override
-	public Optional<IDocument> importDocument(String title, String pathTofile) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
-	}
+    @Override
+    public void processEvent(DocumentEvent e) {
+        emitEvent(e); // propaga eventos do documento como LBEvent
+    }
 
-	@Override
-	public void removeDocument(IDocument document) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateDocument(IDocument document, DocumentProperties documentProperties) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<IDocument> getMatches(String regex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder result = new StringBuilder("Library = \n");
-//		for (IDocument doc: TO COMPLETE AND UNCOMMENT THIS CODE) {
-//			result.append(doc + "\n");
-//		}
-		return result + "\n";
-
-	}
-	
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Library = \n");
+        for (IDocument doc : library) {
+            result.append(doc).append("\n");
+        }
+        return result.toString();
+    }
 }
