e0b9acebda0485739d9a5422e59fc3dd22a2277f
“EricksonDagama” <eriksongamamorais@hotmail.com>
2025-03-23 19:29:34 +0000

    1. Implementação Completa de Shelves.java

--- src/leibooks/domain/shelves/Shelves.java
+++ src/leibooks/domain/shelves/Shelves.java
@@ -1,78 +1,78 @@
 package leibooks.domain.shelves;
 
-import java.util.Iterator;
+import java.util.*;
 import java.util.function.Predicate;
 
 import javax.naming.OperationNotSupportedException;
 
 import leibooks.domain.core.ILibrary;
 import leibooks.domain.facade.IDocument;
-import leibooks.domain.facade.events.ShelfEvent;
-import leibooks.utils.Listener;
+import leibooks.domain.facade.events.*;
+import leibooks.utils.AbsSubject;
 
-public class Shelves implements IShelves {
+public class Shelves extends AbsSubject<ShelfEvent> implements IShelves {
+
+	private final ILibrary library;
+	private final Map<String, IShelf> shelfMap = new HashMap<>();
 
 	public Shelves(ILibrary library) {
-		// TODO Auto-generated constructor stub
+		this.library = library;
 	}
 
 	@Override
 	public Iterator<IShelf> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void emitEvent(ShelfEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void registerListener(Listener<ShelfEvent> obs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void unregisterListener(Listener<ShelfEvent> obs) {
-		// TODO Auto-generated method stub
-		
+		return Collections.unmodifiableCollection(shelfMap.values()).iterator();
 	}
 
 	@Override
 	public boolean addNormalShelf(String shelfName) {
-		// TODO Auto-generated method stub
-		return false;
+		if (shelfMap.containsKey(shelfName)) return false;
+		IShelf shelf = new NormalShelf(shelfName);
+		shelfMap.put(shelfName, shelf);
+		emitEvent(new AddShelfEvent(shelf));
+		return true;
 	}
 
 	@Override
 	public boolean addSmartShelf(String shelfName, Predicate<IDocument> criteria) {
-		// TODO Auto-generated method stub
-		return false;
+		if (shelfMap.containsKey(shelfName)) return false;
+		IShelf shelf = new SmartShelf(shelfName, library, criteria);
+		shelfMap.put(shelfName, shelf);
+		emitEvent(new AddShelfEvent(shelf));
+		return true;
 	}
 
 	@Override
 	public void removeShelf(String shelfName) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
-		
+		IShelf shelf = shelfMap.get(shelfName);
+		if (shelf == null) return;
+		if (shelf instanceof UnRemovable) {
+			throw new OperationNotSupportedException("This shelf is unremovable.");
+		}
+		shelfMap.remove(shelfName);
+		emitEvent(new RemoveShelfEvent(shelf));
 	}
 
 	@Override
 	public void removeDocument(String shelfName, IDocument document) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
-		
+		IShelf shelf = shelfMap.get(shelfName);
+		if (shelf != null && shelf.removeDocument(document)) {
+			emitEvent(new RemoveDocumentShelfEvent(shelf, document));
+		}
 	}
 
 	@Override
 	public boolean addDocument(String shelfName, IDocument document) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
+		IShelf shelf = shelfMap.get(shelfName);
+		if (shelf != null) {
+			return shelf.addDocument(document);
+		}
 		return false;
 	}
 
 	@Override
 	public Iterable<IDocument> getDocuments(String shelfName) {
-		// TODO Auto-generated method stub
-		return null;
+		IShelf shelf = shelfMap.get(shelfName);
+		return (shelf != null) ? shelf : Collections.emptyList();
 	}
 }
