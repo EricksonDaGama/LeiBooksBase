168729c950d881145089e8eebbc18791c22662fc
“EricksonDagama” <eriksongamamorais@hotmail.com>
2025-03-23 19:55:49 +0000

    1. Implementação completa da classe ShelvesController

--- src/leibooks/domain/controllers/ShelvesController.java
+++ src/leibooks/domain/controllers/ShelvesController.java
@@ -1,5 +1,7 @@
 package leibooks.domain.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 import javax.naming.OperationNotSupportedException;
@@ -8,97 +10,78 @@ import leibooks.domain.facade.IDocument;
 import leibooks.domain.facade.IShelvesController;
 import leibooks.domain.facade.events.LBEvent;
 import leibooks.domain.facade.events.ShelfEvent;
+import leibooks.domain.shelves.IShelf;
 import leibooks.domain.shelves.IShelves;
+import leibooks.utils.AbsSubject;
 import leibooks.utils.Listener;
 
+public class ShelvesController extends AbsSubject<LBEvent> implements IShelvesController {
 
-public class ShelvesController implements IShelvesController {
-
+	private final IShelves shelves;
 
 	public ShelvesController(IShelves shelves) {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void emitEvent(LBEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void registerListener(Listener<LBEvent> obs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void unregisterListener(Listener<LBEvent> obs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void processEvent(ShelfEvent e) {
-		// TODO Auto-generated method stub
-		
+		this.shelves = shelves;
+		this.shelves.registerListener(this);
 	}
 
 	@Override
 	public boolean addNormalShelf(String name) {
-		// TODO Auto-generated method stub
-		return false;
+		return shelves.addNormalShelf(name);
 	}
 
 	@Override
 	public boolean addSmartShelf(String name, Predicate<IDocument> criteria) {
-		// TODO Auto-generated method stub
-		return false;
+		return shelves.addSmartShelf(name, criteria);
 	}
 
 	@Override
 	public Iterable<String> getShelves() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> names = new ArrayList<>();
+		for (IShelf s : shelves) {
+			names.add(s.getName());
+		}
+		return names;
 	}
 
 	@Override
 	public void remove(String name) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removeDocument(String shelfName, IDocument document) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
-		
+		shelves.removeShelf(name);
 	}
 
 	@Override
 	public boolean addDocument(String shelfName, IDocument document) throws OperationNotSupportedException {
-		// TODO Auto-generated method stub
-		return false;
+		return shelves.addDocument(shelfName, document);
+	}
+
+	@Override
+	public void removeDocument(String shelfName, IDocument document) throws OperationNotSupportedException {
+		shelves.removeDocument(shelfName, document);
 	}
 
 	@Override
 	public Iterable<IDocument> getDocuments(String shelfName) {
-		// TODO Auto-generated method stub
-		return null;
+		return shelves.getDocuments(shelfName);
 	}
-	
+
+	@Override
+	public void processEvent(ShelfEvent e) {
+		emitEvent(e); // propaga eventos da shelf como LBEvent
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder result = new StringBuilder("Shelves=\n");
-//		for (IShelf s: COMPLETE ME AND UNCOMMENT THIS CODE) {
-//			result.append(s + "= [");
-//			StringBuilder shelfInfo = new StringBuilder("");
-//			for (IDocument d: s)
-//				shelfInfo.append(d.getFile() + ", ");
-//			if (shelfInfo.length() == 0)
-//				result.append("] \n");
-//			else
-//				result.append(shelfInfo.substring(0, shelfInfo.length() - 2) + "]\n");
-//		}
-		return result + "\n";
+		for (IShelf s : shelves) {
+			result.append(s.getName()).append(" = [");
+			StringBuilder shelfInfo = new StringBuilder();
+			for (IDocument d : s) {
+				shelfInfo.append(d.getFile().getName()).append(", ");
+			}
+			if (shelfInfo.length() > 0) {
+				result.append(shelfInfo.substring(0, shelfInfo.length() - 2));
+			}
+			result.append("]\n");
+		}
+		return result.toString();
 	}
-
 }
