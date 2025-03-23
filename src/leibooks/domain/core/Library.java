1f191175ac1b3b354da332989997b4a456ae8619
“EricksonDagama” <eriksongamamorais@hotmail.com>
2025-03-23 18:39:58 +0000

    1. implementação completa e revisada da classe Library

--- src/leibooks/domain/core/Library.java
+++ src/leibooks/domain/core/Library.java
@@ -1,67 +1,70 @@
 package leibooks.domain.core;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 import leibooks.domain.facade.DocumentProperties;
 import leibooks.domain.facade.IDocument;
+import leibooks.domain.facade.events.AddDocumentEvent;
 import leibooks.domain.facade.events.DocumentEvent;
+import leibooks.domain.facade.events.RemoveDocumentEvent;
+import leibooks.domain.facade.events.UpdateDocumentPropertiesEvent;
+import leibooks.utils.AbsSubject;
 import leibooks.utils.Listener;
 
-public class Library implements  ILibrary {
+public class Library extends AbsSubject<DocumentEvent> implements ILibrary {
+
+	private final Set<IDocument> documents = new TreeSet<>();
 
 	@Override
 	public Iterator<IDocument> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void emitEvent(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void registerListener(Listener<DocumentEvent> obs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void unregisterListener(Listener<DocumentEvent> obs) {
-		// TODO Auto-generated method stub
-		
+		return Collections.unmodifiableSet(documents).iterator();
 	}
 
 	@Override
 	public int getNumberOfDocuments() {
-		// TODO Auto-generated method stub
-		return 0;
+		return documents.size();
 	}
 
 	@Override
 	public boolean addDocument(IDocument document) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean added = documents.add(document);
+		if (added) {
+			document.registerListener(this); // React to document events
+			emitEvent(new AddDocumentEvent(document));
+		}
+		return added;
 	}
 
 	@Override
 	public void removeDocument(IDocument document) {
-		// TODO Auto-generated method stub
-		
+		if (documents.remove(document)) {
+			document.unregisterListener(this);
+			emitEvent(new RemoveDocumentEvent(document));
+		}
 	}
 
 	@Override
-	public void updateDocument(IDocument document, DocumentProperties documentProperties) {
-		// TODO Auto-generated method stub
-		
+	public void updateDocument(IDocument document, DocumentProperties properties) {
+		document.setTitle(properties.getTitle());
+		document.setAuthor(properties.getAuthor());
+		emitEvent(new UpdateDocumentPropertiesEvent(document));
 	}
 
 	@Override
 	public List<IDocument> getMatches(String regex) {
-		// TODO Auto-generated method stub
-		return null;
+		List<IDocument> matches = new ArrayList<>();
+		Pattern pattern = Pattern.compile(regex);
+		for (IDocument doc : documents) {
+			if (doc.matches(regex)) {
+				matches.add(doc);
+			}
+		}
+		return matches;
 	}
 
+	@Override
+	public void processEvent(DocumentEvent event) {
+		emitEvent(event); // Propaga eventos dos documentos
+	}
 }
