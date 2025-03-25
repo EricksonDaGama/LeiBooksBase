b7fb344008cdf41373d99fdbbd0f04f854b4847f
“EricksonDagama” <eriksongamamorais@hotmail.com>
2025-03-25 22:40:43 +0000

    comentarios eliminados da classe ImageJpegMetadataReader

--- viewers_readers/ImageJpegMetadataReader.java
+++ viewers_readers/ImageJpegMetadataReader.java
@@ -1,16 +1,16 @@
 //REMOVE COMMENTS ONCE THE CLASS AMetadataReader IS DEFINED
 
 import java.io.FileNotFoundException;
-//import java.util.Optional;
+import java.util.Optional;
 
-//import leibooks.domain.metadatareader.AMetadataReader;
+import leibooks.domain.metadatareader.AMetadataReader;
 
-public class ImageJpegMetadataReader {//extends AMetadataReader {
+public class ImageJpegMetadataReader extends AMetadataReader {
 
 	public ImageJpegMetadataReader(String pathToPhotoFile) throws FileNotFoundException {
-		//super(pathToPhotoFile);
-		//numPages = Optional.of(1);
-		//authors = "";	
+		super(pathToPhotoFile);
+		numPages = Optional.of(1);
+		authors = "";
 	}
 
 }
